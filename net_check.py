d0bfe8dac22b7e55f7b5cac0075443f14884bbb7
clearsky <dadgreeneu@outlook.com>
2026-03-28 15:35:54 +0000

    Added counters and fixed file writing logic

--- net_check.py
+++ net_check.py
@@ -3,6 +3,9 @@ import datetime
 
 targets = ["google.com", "github.com", "127.0.0.1"]
 
+up_count = 0
+down_count = 0
+
 print("--- чекаем сеть шо с ней ---")
 
 with open("backup/network_log.txt", "a") as log:
@@ -11,16 +14,30 @@ with open("backup/network_log.txt", "a") as log:
 
     for host in targets:
         
-        response = os.system(f"ping -c 1 -W 2 {host} > /dev/null 2>&1") 
+         response = os.system(f"ping -c 1 -W 2 {host} > /dev/null 2>&1") 
 
         
-        if response == 0:
+         if response == 0:
             status = "Доступен"
-        else:
+            print(f"[+]{host:15} : stably")
+            up_count += 1
+
+         else:
             status = "недоступен"
+            print(f"[!] {host:15} : {status}! Check connection")
+            down_count += 1
 
         
-        print(f"Хост {host:15} : [{status}]")
+         print(f"Хост {host:15} : {status}")
 
         
-        log.write(f"{host:15} : {status}\n")
+         log.write(f"{host:15} : {status}\n")
+
+    summary = f"\nin total: okey: {up_count}, error: {down_count}"
+
+
+    print("-" * 30)
+    print(summary)        
+ 
+    log.write(f"{'-' * 20}{summary}\n")
+
